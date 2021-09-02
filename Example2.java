3148fb6c3b3c60741007177dd3d7ff5a9b89db13
navyask00 <navyask00@gmail.com>
2021-09-02 10:20:25 +0530

    Second Program

---
+++ Example2.java
@@ -0,0 +1,15 @@
+ public class Demo {
+	    int a=100;
+	    static int b=200;
+	    public static void main(String[] args) {
+	        Demo s=new Demo();
+	        s.a=s.a+b;
+	        b=s.a-b;
+	        s.a=s.a-b;
+	        System.out.println(s.a);
+	        System.out.println(b);
+	           
+	       
+	    }
+	}
+
